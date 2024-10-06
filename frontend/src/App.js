da09943a64693bef2be47922ce0b27aa212e87bb
Simon Luthe <simonluthe@MacBook-Air-von-Simon.local>
2024-10-06 18:05:29 +0200

    fick

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1636,9 +1636,8 @@ function AppContent() {
     <div className="flex flex-col-mobile justify-between items-center mb-8">
     <h1 className="text-3xl font-bold mb-4 sm:mb-0">Fahrtenabrechnung</h1>
     <div className="flex space-x-2">
-    <button 
-    console.log('Profil button clicked');
-    onClick={() => setIsProfileModalOpen(true)} 
+    <button
+    onClick={() => setIsProfileModalOpen(true)}
     className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full-mobile"
     >
     Profil
