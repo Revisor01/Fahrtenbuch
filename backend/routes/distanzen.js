62f8070346153767e669f4ceebba7129cb83342f
Revisor01 <mail@simonluthe.de>
2025-02-05 16:15:14 +0100

    routen reihenfolgen korrigiert.

--- backend/routes/distanzen.js
+++ backend/routes/distanzen.js
@@ -3,14 +3,12 @@ const router = express.Router();
 const distanzController = require('../controllers/distanzController');
 const { authMiddleware } = require('../middleware/authMiddleware');
 
-// Fügen Sie authMiddleware zu allen Routen hinzu
 router.use(authMiddleware);
 
 router.post('/', distanzController.createOrUpdateDistanz);
 router.get('/between', distanzController.getDistanz);
 router.get('/', distanzController.getAllDistanzen);
 router.put('/:id', distanzController.updateDistanz);
-router.get('/autosplit', distanzController.getAutoSplitDistance);
 router.delete('/:id', distanzController.deleteDistanz);
 
 module.exports = router;
