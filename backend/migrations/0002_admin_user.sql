679cc5aba27080b5dd63c9e17f0ec11b880c0e9f
Revisor01 <mail@simonluthe.de>
2025-02-07 22:06:48 +0100

    sjaidw

--- backend/migrations/0002_admin_user.sql
+++ backend/migrations/0002_admin_user.sql
@@ -1,3 +1,8 @@
+-- Setze Standardwerte aus ENV-Variablen (als Strings behandeln)
+SET @default_erstattung_traeger = '${DEFAULT_ERSTATTUNG_TRAEGER}';
+SET @default_erstattung_mitfahrer = '${DEFAULT_ERSTATTUNG_MITFAHRER}';
+SET @default_erstattung_datum = '${DEFAULT_ERSTATTUNG_DATUM}';
+
 -- Insert Initial Admin User
 INSERT INTO users (username, password, role, email_verified)
 SELECT * FROM (SELECT
@@ -24,7 +29,7 @@ AFTER INSERT ON users
 FOR EACH ROW
 BEGIN
     INSERT INTO mitfahrer_erstattung (user_id, betrag, gueltig_ab)
-    VALUES (NEW.id, '${DEFAULT_ERSTATTUNG_MITFAHRER}', '${DEFAULT_ERSTATTUNG_DATUM}');
+    VALUES (NEW.id, @default_erstattung_mitfahrer, @default_erstattung_datum);
 END;
 
 DROP TRIGGER IF EXISTS after_abrechnungstraeger_create;
@@ -34,5 +39,5 @@ AFTER INSERT ON abrechnungstraeger
 FOR EACH ROW
 BEGIN
     INSERT INTO erstattungsbetraege (abrechnungstraeger_id, betrag, gueltig_ab)
-    VALUES (NEW.id, '${DEFAULT_ERSTATTUNG_TRAEGER}', '${DEFAULT_ERSTATTUNG_DATUM}');
+    VALUES (NEW.id, @default_erstattung_traeger, @default_erstattung_datum);
 END;
