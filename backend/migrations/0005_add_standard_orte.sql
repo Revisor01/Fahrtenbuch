91ebf225aec19d2f4b1b222b52d14eec44461d61
Revisor01 <mail@simonluthe.de>
2025-02-13 00:40:28 +0100

    standrd

--- backend/migrations/0005_add_standard_orte.sql
+++ backend/migrations/0005_add_standard_orte.sql
@@ -1,38 +1,33 @@
--- Migration: Standardorte hinzufügen
--- Datum: [Datum der Migration]
+-- Trigger: after_user_create_orte
+DROP TRIGGER IF EXISTS after_user_create_orte;
 
--- Erster Standardort
-INSERT INTO orte (name, adresse, ist_wohnort, ist_dienstort, ist_kirchspiel, usage_count, user_id)
-SELECT 
-    '${STANDARD_ORT_1_NAME}', 
-    '${STANDARD_ORT_1_ADRESSE}', 
-    0, 
-    0, 
-    0, 
-    0, 
-    NULL
-FROM DUAL
-WHERE '${STANDARD_ORT_1_NAME}' != ''
-AND NOT EXISTS (
-    SELECT 1 FROM orte 
-    WHERE name = '${STANDARD_ORT_1_NAME}' 
-    AND adresse = '${STANDARD_ORT_1_ADRESSE}'
-);
+CREATE TRIGGER after_user_create_orte AFTER INSERT ON users FOR EACH ROW
+BEGIN
+    -- Erster Standardort
+    IF LENGTH(TRIM('${STANDARD_ORT_1_NAME}')) > 0 THEN
+        INSERT INTO orte (name, adresse, ist_wohnort, ist_dienstort, ist_kirchspiel, usage_count, user_id)
+        VALUES (
+            '${STANDARD_ORT_1_NAME}', 
+            '${STANDARD_ORT_1_ADRESSE}', 
+            0, 
+            0, 
+            0, 
+            0, 
+            NEW.id
+        );
+    END IF;
 
--- Zweiter Standardort
-INSERT INTO orte (name, adresse, ist_wohnort, ist_dienstort, ist_kirchspiel, usage_count, user_id)
-SELECT 
-    '${STANDARD_ORT_2_NAME}', 
-    '${STANDARD_ORT_2_ADRESSE}', 
-    0, 
-    0, 
-    0, 
-    0, 
-    NULL
-FROM DUAL
-WHERE '${STANDARD_ORT_2_NAME}' != ''
-AND NOT EXISTS (
-    SELECT 1 FROM orte 
-    WHERE name = '${STANDARD_ORT_2_NAME}' 
-    AND adresse = '${STANDARD_ORT_2_ADRESSE}'
-);
+    -- Zweiter Standardort
+    IF LENGTH(TRIM('${STANDARD_ORT_2_NAME}')) > 0 THEN
+        INSERT INTO orte (name, adresse, ist_wohnort, ist_dienstort, ist_kirchspiel, usage_count, user_id)
+        VALUES (
+            '${STANDARD_ORT_2_NAME}', 
+            '${STANDARD_ORT_2_ADRESSE}', 
+            0, 
+            0, 
+            0, 
+            0, 
+            NEW.id
+        );
+    END IF;
+END;
